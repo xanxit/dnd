deb941ba71fdcfedf1735fb1b06d2dec78556e26
Sanchit Hajela <sanchit10hajela@gmail.com>
2024-02-25 16:02:50 +0530

    feat: persist redux state on refresh

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,6 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
 import rootReducer from "./reducers"
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Specify the reducers you want to persist
+  whitelist: ['rootReducer'], // In this example, we persist the 'user' reducer
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
   reducer: rootReducer
 })
+export const persistor = persistStore(store);
